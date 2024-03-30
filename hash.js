e2f216d4f1526c62965c49b9a0e0c290a4b36553
Samuel <samuelrivba@gmail.com>
2024-03-29 23:02:40 -0500

    Create a HashTable class

--- hash.js
+++ hash.js
@@ -11,3 +11,22 @@ console.log(user.age); // O(1)
 user.spell = "Abra Kadabra"; // O(1)
 console.log(user.spell);
 user.scream(); // O(1)
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+}
+
+const myHashTable = new HashTable(50);
+console.log(myHashTable);
+// myHashTable.set("grapes", 10000);
+// myHashTable.get("grapes");
